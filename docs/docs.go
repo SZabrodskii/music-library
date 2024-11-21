c73e43fa5728f76376821b1bb3c6ead146049dc3
SZabrodskii <Stanislas.zabrodsky@icloud.com>
2024-11-21 16:16:13 -0300

    feat: add Swagger documentation

---
+++ docs/docs.go
@@ -0,0 +1,8 @@
+package docs
+
+// @title Music Library API
+// @version 1.0
+// @description API for managing a music library.
+// @host localhost:8080
+// @BasePath /api/v1
+// @schemes http https
