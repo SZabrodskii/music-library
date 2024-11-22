93873c2d6066ac6cc8cb3531c965e5e42d189242
SZabrodskii <Stanislas.zabrodsky@icloud.com>
2024-11-22 18:06:34 -0300

    feat: add cache utility

--- utils/cache/cache.go
+++ utils/cache/cache.go
@@ -2,20 +2,21 @@ package cache
 
 import (
 	"context"
+	"github.com/SZabrodskii/music-library/utils/config"
 	"github.com/go-redis/redis/v8"
 	"go.uber.org/zap"
-	"music-library/gateway/config"
 	"time"
 )
 
-var logger *zap.Logger
-var rdb *redis.Client
-var localCache map[string]interface{}
+type Cache struct {
+	Logger     *zap.Logger
+	rdb        *redis.Client
+	localCache map[string]interface{}
+}
 
-func NewCache() *Cache {
-	logger, _ = zap.NewProduction()
-	localCache = make(map[string]interface{})
-	rdb = redis.NewClient(&redis.Options{
+func NewCache(logger *zap.Logger) *Cache {
+	localCache := make(map[string]interface{})
+	rdb := redis.NewClient(&redis.Options{
 		Addr: config.GetEnv("REDIS_URL"),
 	})
 	return &Cache{
@@ -25,14 +26,8 @@ func NewCache() *Cache {
 	}
 }
 
-type Cache struct {
-	Logger     *zap.Logger
-	rdb        *redis.Client
-	localCache map[string]interface{}
-}
-
 func (c *Cache) GetFromCache(key string) (interface{}, bool) {
-	if val, ok := localCache[key]; ok {
+	if val, ok := c.localCache[key]; ok {
 		return val, true
 	}
 	ctx := context.Background()
@@ -40,39 +35,38 @@ func (c *Cache) GetFromCache(key string) (interface{}, bool) {
 	if err == redis.Nil {
 		return nil, false
 	} else if err != nil {
-		logger.Error("Failed to get from Redis", zap.Error(err))
+		c.Logger.Error("Failed to get from Redis", zap.Error(err))
 		return nil, false
 	}
 	return val, true
 }
 
 func (c *Cache) SetToCache(key string, value interface{}, ttl time.Duration) {
-	localCache[key] = value
+	c.localCache[key] = value
 	ctx := context.Background()
 
 	err := c.rdb.Set(ctx, key, value, ttl).Err()
 	if err != nil {
-		logger.Error("Failed to set to Redis", zap.Error(err))
+		c.Logger.Error("Failed to set to Redis", zap.Error(err))
 	}
-
 }
 
 func (c *Cache) DeleteFromCache(key string) {
-	delete(localCache, key)
+	delete(c.localCache, key)
 	ctx := context.Background()
 
 	err := c.rdb.Del(ctx, key).Err()
 	if err != nil {
-		logger.Error("Failed to delete from Redis", zap.Error(err))
+		c.Logger.Error("Failed to delete from Redis", zap.Error(err))
 	}
 }
 
 func (c *Cache) ClearCache() {
-	localCache = make(map[string]interface{})
+	c.localCache = make(map[string]interface{})
 	ctx := context.Background()
 
-	err := rdb.FlushDB(ctx).Err()
+	err := c.rdb.FlushDB(ctx).Err()
 	if err != nil {
-		logger.Error("Failed to clear Redis", zap.Error(err))
+		c.Logger.Error("Failed to clear Redis", zap.Error(err))
 	}
 }
