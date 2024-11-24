13e582558addf29f8c94baa888874d29cdf7c57a
SZabrodskii <Stanislas.zabrodsky@icloud.com>
2024-11-24 16:24:02 -0300

    chore: add middleware for caching recall for router

--- gateway/handlers/router.go
+++ gateway/handlers/router.go
@@ -3,28 +3,35 @@ package handlers
 import (
 	"github.com/SZabrodskii/music-library/song-service/services"
 	"github.com/SZabrodskii/music-library/utils/cache"
+	"github.com/SZabrodskii/music-library/utils/middleware"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 )
 
-func NewRouter(cache *cache.Cache, service *services.SongService, logger *zap.Logger) *gin.Engine {
-	router := gin.Default()
+type Router struct {
+	engine *gin.Engine
+}
 
-	router.GET("/api/v1/songs", func(c *gin.Context) {
-		GetSongs(c, cache, service, logger)
+func NewRouter(cache *cache.Cache, client *services.Client, logger *zap.Logger) *Router {
+	router := gin.New()
+	router.Use(gin.Recovery())
+	router.Use(func(ctx *gin.Context) {
+		// Add time.Since to logger
+		ctx.Next()
+		logger.Info("New request", zap.String("method", ctx.Request.Method), zap.String("path", ctx.Request.URL.Path), zap.Any("query", ctx.Request.URL.Query()))
 	})
-	router.GET("/api/v1/songs/:songId/text", func(c *gin.Context) {
-		GetSongText(c, cache, service, logger)
-	})
-	router.DELETE("/api/v1/songs/:songId", func(c *gin.Context) {
-		DeleteSong(c, cache, service, logger)
-	})
-	router.PATCH("/api/v1/songs/:songId", func(c *gin.Context) {
-		UpdateSong(c, cache, service, logger)
-	})
-	router.POST("/api/v1/songs", func(c *gin.Context) {
-		AddSong(c, cache, service, logger)
-	})
+	router.Use(middleware.CacheMiddleware(cache))
+	songHandler := NewSongHandler(cache, client, logger)
 
-	return router
+	router.GET("/api/v1/songs", songHandler.GetSongs)
+	router.GET("/api/v1/songs/:songId/text", songHandler.GetSongText)
+	router.DELETE("/api/v1/songs/:songId", songHandler.DeleteSong)
+	router.PATCH("/api/v1/songs/:songId", songHandler.UpdateSong)
+	router.POST("/api/v1/songs", songHandler.AddSong)
+
+	return &Router{engine: router}
+}
+
+func (r *Router) Start() {
+	r.engine.Run(":8080")
 }
