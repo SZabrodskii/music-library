e5db7422519e1524dcac438bb238bbb111578257
SZabrodskii <Stanislas.zabrodsky@icloud.com>
2024-11-27 11:58:28 -0300

    feat: add wrapping errors

--- utils/providers/rabbitmq_provider.go
+++ utils/providers/rabbitmq_provider.go
@@ -1,9 +1,10 @@
 package providers
 
 import (
+	"fmt"
+	"github.com/SZabrodskii/music-library/utils"
 	"github.com/streadway/amqp"
 	"go.uber.org/zap"
-	"os"
 )
 
 type RabbitMQProviderConfig struct {
@@ -12,7 +13,7 @@ type RabbitMQProviderConfig struct {
 
 func NewRabbitMQProviderConfig() *RabbitMQProviderConfig {
 	return &RabbitMQProviderConfig{
-		URL: os.Getenv("RABBITMQ_URL"),
+		URL: utils.GetEnv("RABBITMQ_URL", "amqp://guest:guest@rabbitmq:5672/"),
 	}
 }
 
@@ -22,20 +23,20 @@ type RabbitMQProvider struct {
 	ch     *amqp.Channel
 }
 
-func NewRabbitMQProvider(logger *zap.Logger, config *RabbitMQProviderConfig) *RabbitMQProvider {
+func NewRabbitMQProvider(logger *zap.Logger, config *RabbitMQProviderConfig) (*RabbitMQProvider, error) {
 	conn, err := amqp.Dial(config.URL)
 	if err != nil {
-		logger.Fatal("Failed to connect to RabbitMQ", zap.Error(err))
+		return nil, fmt.Errorf("failed to connect to rabbitmq: %w", err)
 	}
 	ch, err := conn.Channel()
 	if err != nil {
-		logger.Fatal("Failed to open a channel", zap.Error(err))
+		return nil, fmt.Errorf("failed to open a channel: %w", err)
 	}
 	return &RabbitMQProvider{
 		logger: logger,
 		conn:   conn,
 		ch:     ch,
-	}
+	}, nil
 }
 
 func (r *RabbitMQProvider) GetConnection() *amqp.Connection {
@@ -48,10 +49,10 @@ func (r *RabbitMQProvider) GetChannel() *amqp.Channel {
 
 func (r *RabbitMQProvider) Publish(queueName string, body []byte) error {
 	err := r.ch.Publish(
-		"",        // exchange
-		queueName, // routing key
-		false,     // mandatory
-		false,     // immediate
+		"",
+		queueName,
+		false,
+		false,
 		amqp.Publishing{
 			ContentType: "application/json",
 			Body:        body,
@@ -65,13 +66,13 @@ func (r *RabbitMQProvider) Publish(queueName string, body []byte) error {
 
 func (r *RabbitMQProvider) Consume(queueName string, consumer func(d amqp.Delivery)) error {
 	msgs, err := r.ch.Consume(
-		queueName, // queue
-		"",        // consumer
-		true,      // auto-ack
-		false,     // exclusive
-		false,     // no-local
-		false,     // no-wait
-		nil,       // args
+		queueName,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
 	)
 	if err != nil {
 		r.logger.Error("Failed to register a consumer", zap.Error(err))
