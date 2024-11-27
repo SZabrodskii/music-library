605ad9ee433c59d9b94ae754ef33db95cf92d51e
SZabrodskii <Stanislas.zabrodsky@icloud.com>
2024-11-27 11:53:30 -0300

    feat: add logger

--- gateway/main.go
+++ gateway/main.go
@@ -2,33 +2,29 @@ package main
 
 import (
 	"github.com/SZabrodskii/music-library/gateway/handlers"
+	_ "github.com/SZabrodskii/music-library/song-service/migrations"
 	"github.com/SZabrodskii/music-library/utils/providers"
 	"github.com/SZabrodskii/music-library/utils/services"
 	"go.uber.org/fx"
-	"go.uber.org/zap"
-
-	_ "github.com/SZabrodskii/music-library/song-service/migrations"
 )
 
 func main() {
 	app := fx.New(
 		fx.Provide(
-			NewLogger,
+			providers.NewLoggerProviderConfig,
+			providers.NewLogger,
+			providers.UseLogger,
 			providers.NewCacheProvider,
-			providers.NewRabbitMQProvider,
+			services.NewSongServiceClientConfig,
 			services.NewSongServiceClient,
+			providers.NewRedisProviderConfig,
+			providers.NewRedisProvider,
 			handlers.NewSongHandler,
 			handlers.NewRouter,
 		),
 		fx.Invoke(startServer),
 	)
-
 	app.Run()
-}
-
-func NewLogger() *zap.Logger {
-	logger, _ := zap.NewProduction()
-	return logger
 }
 
 func startServer(router *handlers.Router) {
